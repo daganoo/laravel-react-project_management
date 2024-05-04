4b110ae2ccc3e8672c72dc59144b40a0d02719d2
Marouane Dagana <136081716+daganoo@users.noreply.github.com>
2024-05-04 17:01:19 +0100

    detete withd

--- resources/js/Components/ApplicationLogo.jsx
+++ resources/js/Components/ApplicationLogo.jsx
@@ -1,7 +1,7 @@
 export default function ApplicationLogo(props) {
     return (
 
-        <img src="/img/icon1.svg" alt="" width="100px" />
+        <img src="/img/icon1.svg" alt=""  />
         
     );
 }
